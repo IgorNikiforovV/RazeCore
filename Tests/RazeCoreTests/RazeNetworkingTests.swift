156f387f5695f55f9c8c8b21007fc4c772d826be
Igor <igor.nikiforov@domru.ru>
2021-03-29 10:05:07 +0500

    Added second Networking API for sending data

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -16,6 +16,16 @@ final class RazeNetworkingTests: XCTestCase {
         func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
             completionHandler(data, error)
         }
+
+        func post(with request: URLRequest,
+                  completionHandler: @escaping (Data?, Error?) -> Void) {
+            completionHandler(data, error)
+        }
+    }
+
+    struct MockData: Codable, Equatable {
+        var id: Int
+        var name: String
     }
 
     func testLoadDataCall() {
@@ -38,8 +48,33 @@ final class RazeNetworkingTests: XCTestCase {
         wait(for: [expectation], timeout: 5)
     }
 
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "David")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Sent data")
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+                case .success(let returneData):
+                    let returnedObject =
+                        try? JSONDecoder().decode(MockData.self, from: returneData)
+                    XCTAssertEqual(returnedObject, sampleObject)
+                    break
+                case .failure(let error):
+                    XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+
     static var allTests = [
-        ("testLoadDataCall", testLoadDataCall)
+        ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall)
     ]
 
 }
